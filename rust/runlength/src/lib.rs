aba06c0ad73f5de69cdd641fad6a51746ec2b065
artemetra <artemlukin15@gmail.com>
2022-04-23 23:34:14 +0200

    rust version: - made fully generic - a bit more memory efficient to use u8 to store repetitions - ensured there are no overflows - fixed pretty printing - removed костыль - ensure the input vector is bigger then two

--- rust/runlength/src/lib.rs
+++ rust/runlength/src/lib.rs
@@ -1,58 +1,58 @@
 use std::fmt;
 
 #[derive(Clone)]
-struct ByteCount {
-    pub byte: u8,
-    pub num: u32,
+struct TCount<T>
+{
+    pub t: T,
+    pub num: u8,
 }
 
-impl ByteCount {
-    pub fn new(byte: u8, num: u32) -> ByteCount {
-        ByteCount {
-            byte, num
-        }
-    }
-}
-
-impl fmt::Display for ByteCount {
+impl<T> fmt::Display for TCount<T> 
+where T: fmt::Display
+{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self.num {
-            0 => write!(f, "{}", self.byte),
-            _ => write!(f, "{}[{}]", self.byte, self.num),
+            1 => write!(f, "{}", self.t),
+            _ => write!(f, "({})[{}]", self.t, self.num),
         }
     }
 }
 
-pub struct RunLengthEncoded {
-    elements: Vec<ByteCount>,
+pub struct RunLengthEncoded<T> {
+    elements: Vec<TCount<T>>,
 }
 
-impl RunLengthEncoded {
-    pub fn encode(string: &str) -> RunLengthEncoded {
-        let mut result = vec![ByteCount::new(0, 0)];
+impl<T> RunLengthEncoded<T> 
+where T: PartialEq + Copy,
+{
+    pub fn encode(input: Vec<T>) -> Result<RunLengthEncoded<T>, &'static str> {
+        if input.len() < 2 {
+            return Err("input vector length is < 2")
+        }
+
+        let mut result = vec![TCount{t: input[0], num: 1}];
         let mut idx = 0;
-        for by in string.bytes() {
+        for t in input[1..].into_iter() {
             let mut current = &mut result[idx];
-            if current.byte == by {
+
+            if current.t == *t && current.num != 255 {
                 current.num += 1;
             } else {
-                result.push(ByteCount::new(by, 1));
+                result.push(TCount{t: *t, num: 1});
                 idx += 1;
             }
-            
         }
 
-        match result[0].byte {
-            0 => RunLengthEncoded{elements: result[1..].to_vec()},
-            _ => RunLengthEncoded{elements: result}
-        }
+        Ok(RunLengthEncoded{elements: result})
     }
+    
 }
 
-impl fmt::Display for RunLengthEncoded {
+impl<T> fmt::Display for RunLengthEncoded<T>
+where T: fmt::Display {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for elem in &self.elements {
-            write!(f, "{} ", elem)?;
+            write!(f, "{}", elem)?;
         }
         write!(f, "")
     }
--- rust/runlength/src/main.rs
+++ rust/runlength/src/main.rs
@@ -5,6 +5,6 @@ use std::env;
 fn main() {
     let args_vec: Vec<_> = env::args().collect();
     let arg = args_vec.get(1).unwrap();
-    let encoded = RunLengthEncoded::encode(&arg);
-    println!("{}", encoded)
+    let encoded = RunLengthEncoded::encode((*arg).chars().collect());
+    println!("{}", encoded.unwrap());
 }
